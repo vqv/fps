e6e9ca8a604c7c2c80a4428e3f7f813fed6dcb40
Vince Vu <vqv@me.com>
2014-08-08 17:04:01 -0400

    Optimizations from previous commit incorporated into non-block version of simplex()

--- src/simplex.cpp
+++ src/simplex.cpp
@@ -52,8 +52,22 @@ uword simplex(vec& x, double d, bool interior) {
     return rank;
   }
 
-  // Construct vector of knots, sorted in ascending order
-  vec knots = unique(join_vert(x - 1.0, x));
+  // Let x(j) denote the jth largest x.
+  // The root of the piecewise linear function must lie in the interval
+  // [x(d) - 1, x(d-1)]
+  // The knots of the function consist of the values of x and x - 1
+  std::set<double> knots(x.begin(), x.end());
+
+  // Construct knots corresponding to x-1 -- sorted in descending order
+  std::vector<double> tmp(std::min(knots.size(), (size_t) std::ceil(d)));
+  auto i = knots.crbegin();
+  for (auto& t : tmp) { t = *i++ - 1; }
+
+  // Eliminate any knots that are smaller than x(d) - 1
+  knots.erase(knots.begin(), knots.lower_bound(*tmp.crbegin()));
+
+  // Insert x-1 knots
+  for (const auto& t : tmp) { knots.insert(knots.begin(), t); }
 
   // Find the left-most knot whose function value is < d 
   // This knot is the right endpoint of the interval containing 
