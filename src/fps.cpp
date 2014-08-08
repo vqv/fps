vqv/fps

commit d254891b4adfcbadc582d534d826c0e78b0ad4ad

Fixed style of one line if statements and other minor formatting changes

src/fps.cpp
@@ -53,11 +53,11 @@ void compute_lambdarange(const GraphSeq& gs,
   // Find the first knot at which the maximum block size exceeds maxnvar
   auto i = std::lower_bound(gs.cbegin(), gs.cend(), 2 * maxnvar, 
     [](const GraphSeq::value_type& a, const int& b) {
-      arma::uword maxsize = 0;
+      uword maxsize = 0;
       for (const auto& j : a.second) {
         maxsize = std::max(maxsize, j.second.n_elem);
       }
-      return maxsize < (arma::uword) b;
+      return maxsize < (uword) b;
     }
   );
   if (i != gs.cbegin()) { --i; }
@@ -142,11 +142,11 @@ List fps(NumericMatrix S, double ndim, int nsol = 50,
          int maxiter = 100, double tolerance = 1e-3, int verbose = 0) {
 
   // Sanity checks
-  if (S.nrow() < 2) stop("Expected S to be a matrix");
-  if (ndim <= 0.0 || ndim >= S.nrow()) stop("Expected ndim to be between 0.0 and the number of rows/columns of S");
-  if (nsol < 1) stop("Expected nsol > 0");
-  if (maxiter < 1) stop("Expected maxiter > 0");
-  if (tolerance <= 0.0) stop("Expected tolerance > 0");
+  if (S.nrow() < 2) { stop("Expected S to be a matrix"); }
+  if (ndim <= 0.0 || ndim >= S.nrow()) { stop("Expected 0 < ndim < nrow(S)"); }
+  if (nsol < 1) { stop("Expected nsol > 0");
+  if (maxiter < 1) { stop("Expected maxiter > 0");
+  if (tolerance <= 0.0) { stop("Expected tolerance > 0");
 
   // Wrap the input matrix with an arma::mat
   const mat _S(S.begin(), S.nrow(), S.ncol(), false);
@@ -193,7 +193,7 @@ List fps(NumericMatrix S, double ndim, int nsol = 50,
 
   // Outer loop to compute solution path
   for (int i = 0; i < nsol; i++) {
-    if (verbose > 0) Rcout << ".";
+    if (verbose > 0) { Rcout << "."; }
 
     // Find active vertex partition and construct block matrix
     const GraphSeq::partition_t& active = gs.get_active(_lambda[i]);
@@ -224,11 +224,11 @@ List fps(NumericMatrix S, double ndim, int nsol = 50,
     varexplained(i) = dot(block_S, block_z);
     _leverage.col(i) = _p.diag();
 
-    if (verbose > 1) Rcout << niter[i];
-    if (verbose > 2) Rcout << "(" << admm_penalty << ")";
+    if (verbose > 1) { Rcout << niter[i]; }
+    if (verbose > 2) { Rcout << "(" << admm_penalty << ")"; }
   }
 
-  if (verbose > 0) Rcout << std::endl;
+  if (verbose > 0) { Rcout << std::endl; }
 
   // Return
   List out = List::create(

src/svps.cpp
@@ -128,13 +128,13 @@ List svps(NumericMatrix x, double ndim,
           int maxiter = 100, double tolerance = 1e-3, int verbose = 0) {
 
   // Sanity checks
-  if (x.ncol() < 2 || x.nrow() < 2) stop("Expected x to be a matrix");
+  if (x.ncol() < 2 || x.nrow() < 2) { stop("Expected x to be a matrix"); }
   if (ndim <= 0.0 || ndim >= std::min(x.nrow(), x.ncol())) {
-    stop("Expected ndim to be between 0 and the number of rows/columns of S");
+    stop("Expected 0 < ndim < min(dim(x))");
   }
-  if (nsol < 1) stop("Expected nsol > 0");
-  if (maxiter < 1) stop("Expected maxiter > 0");
-  if (tolerance <= 0.0) stop("Expected tolerance > 0");
+  if (nsol < 1) { stop("Expected nsol > 0"); }
+  if (maxiter < 1) { stop("Expected maxiter > 0"); }
+  if (tolerance <= 0.0) { stop("Expected tolerance > 0"); }
 
   // Map x to an arma::mat
   const mat _x(x.begin(), x.nrow(), x.ncol(), false);
@@ -185,7 +185,7 @@ List svps(NumericMatrix x, double ndim,
 
   // Outer loop to compute solution path
   for (int i = 0; i < nsol; ++i) {
-    if (verbose > 0) Rcout << ".";
+    if (verbose > 0) { Rcout << "."; }
 
     // Find active vertex partition and construct block matrix
     const BiGraphSeq::partition_t& active = gs.get_active(_lambda[i]);
@@ -218,11 +218,11 @@ List svps(NumericMatrix x, double ndim,
     _leverage_row.col(i) = vectorise(sum(square(_p), 1));
     _leverage_col.col(i) = vectorise(sum(square(_p), 0));
 
-    if (verbose > 1) Rcout << niter[i];
-    if (verbose > 2) Rcout << "(" << admm_penalty << ")";
+    if (verbose > 1) { Rcout << niter[i]; }
+    if (verbose > 2) { Rcout << "(" << admm_penalty << ")"; }
   }
 
-  if (verbose > 0) Rcout << std::endl;
+  if (verbose > 0) { Rcout << std::endl; }
 
   // Return
   List out = List::create(

commit 5f2a38f379eccdf78126bdf7be0d5d6e6ed73f62

Corrected typos that broke compilation in commit d254891

src/fps.cpp
@@ -51,7 +51,7 @@ void compute_lambdarange(const GraphSeq& gs,
   }
 
   // Find the first knot at which the maximum block size exceeds maxnvar
-  auto i = std::lower_bound(gs.cbegin(), gs.cend(), 2 * maxnvar, 
+  auto i = std::lower_bound(gs.cbegin(), gs.cend(), maxnvar, 
     [](const GraphSeq::value_type& a, const int& b) {
       uword maxsize = 0;
       for (const auto& j : a.second) {
@@ -144,9 +144,9 @@ List fps(NumericMatrix S, double ndim, int nsol = 50,
   // Sanity checks
   if (S.nrow() < 2) { stop("Expected S to be a matrix"); }
   if (ndim <= 0.0 || ndim >= S.nrow()) { stop("Expected 0 < ndim < nrow(S)"); }
-  if (nsol < 1) { stop("Expected nsol > 0");
-  if (maxiter < 1) { stop("Expected maxiter > 0");
-  if (tolerance <= 0.0) { stop("Expected tolerance > 0");
+  if (nsol < 1) { stop("Expected nsol > 0"); }
+  if (maxiter < 1) { stop("Expected maxiter > 0"); }
+  if (tolerance <= 0.0) { stop("Expected tolerance > 0"); }
 
   // Wrap the input matrix with an arma::mat
   const mat _S(S.begin(), S.nrow(), S.ncol(), false);